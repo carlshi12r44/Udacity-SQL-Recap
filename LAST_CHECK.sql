5beb6ebb4ff5c58131a949e41144b20be41700ad
carlshi12r44 <coin123fs@gmail.com>
2020-01-25 20:52:57 -0500

    Edit Last Check

--- LAST_CHECK.sql
+++ LAST_CHECK.sql
@@ -85,9 +85,9 @@ WHERE a.id = '1001';
 -- Find all the orders that occurred in 2015. Your final table should have 4 columns: occurred_at,
 -- account name, order total, and order total_amt_usd.
 
-SELECT o.occurred_at order_time, o.total order_total, o.total_amt_usd total_amout
-FROM orders o
-JOIN accounts a
+SELECT o.occurred_at order_time, a.name account_name, o.total order_total, o.total_amt_usd total_amout
+FROM accounts a 
+JOIN orders o
 ON o.account_id = a.id 
 WHERE o.occurred_at BETWEEN '01-01-2015' AND '01-01-2016'
 ORDER BY o.occurred_at DESC;
